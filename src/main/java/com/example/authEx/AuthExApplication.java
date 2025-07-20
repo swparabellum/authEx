8e1798d1f59d21e92481cbccb127bcc2862ac4cd
swparabellum <swparabellum@gmail.com>
2025-07-20 16:27:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/authEx/AuthExApplication.java
@@ -0,0 +1,13 @@
+package com.example.authEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authEx/AuthExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
