cbb81537936745fd6180148d40dc55131db1a82e
swparabellum <swparabellum@gmail.com>
2025-07-22 19:49:29 +0900

    기능추가: 로그아웃 구현

--- src/main/java/com/example/authEx/config/SecurityConfig.java
+++ src/main/java/com/example/authEx/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.example.authEx.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +20,10 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
+        HeaderWriterLogoutHandler clearSiteData = new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.COOKIES));
+
+
+        return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
@@ -24,10 +31,20 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .failureUrl("/login?error=true")
                         .defaultSuccessUrl("/home")
                         .permitAll()
-                );
-        return http.build();
+                )
+                .logout((logout) ->
+                        logout
+                                .logoutRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET,"/logout") )
+                                .logoutSuccessUrl("/login")
+                                .addLogoutHandler(clearSiteData)
+                                .invalidateHttpSession(true))
+                .build();
+
     }
 
 
